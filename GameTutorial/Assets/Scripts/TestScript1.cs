b12d64f3e7527c797ccef322f48e99778c7db353
Gerald McAuley <geraldmcauley@gmail.com>
2020-01-06 21:01:49 +0000

    first commit

---
+++ GameTutorial/Assets/Scripts/TestScript1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        System.Console.WriteLine (	"test123123");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
